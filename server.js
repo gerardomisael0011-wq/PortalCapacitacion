2d40f4427ea96cb7efe84533aeefa87ebf168af8
Gerardo <gerardomisael0011@gmail.com>
2026-09-21 15:29:50 -0600

    Actualizar cursos y base de datos

--- server.js
+++ server.js
@@ -255,7 +255,7 @@ db.serialize(() => {
 
 // 2. MAPA DE ASIGNACIONES: Define exactamente qué ID(s) de curso le tocan a cada Nómina
 const cursosPorNomina = {
-    '2887': [1, 5, 6, 11, 12, 13], // Gerardo verá solo los cursos 1, 2 y 4
+    '2887': [1, 5, 6, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21], // Gerardo verá solo los cursos 1, 2 y 4
     '1011': [2, 3],    // Gilberto verá solo el 2 y el 3
     '1018': [1, 3, 4],  // Norma verá solo el 1, 3 y 4
     '1037': [2, 3],
